db44b21fd4f3a9690828897048c57be99217de83
luoxiangyu02 <1962554725@qq.com>
2023-04-17 14:50:06 +0800

    Initial commit

---
+++ src/main/java/reggie/service/impl/EmployeeServicecImpl.java
@@ -0,0 +1,11 @@
+package reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import reggie.entity.Employee;
+import reggie.mapper.EmployeeMapper;
+import reggie.service.EmployeeService;
+
+@Service
+public class EmployeeServicecImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+}
