1009d125fc4f76ae58faf510d104a9b2d93a6ba1
luoxiangyu02 <1962554725@qq.com>
2023-05-18 21:16:18 +0800

    day2 员工信息

--- src/main/java/reggie/ReggieApplication.java
+++ src/main/java/reggie/ReggieApplication.java
@@ -3,8 +3,11 @@ package reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan //因为有了过滤器 所以一定要添加这个注解
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
